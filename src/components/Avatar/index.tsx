05c6dc47326ccc0a79cf7059b47de27f47111867
ATCamposs <andre.tatibano@gmail.com>
2021-07-25 20:05:17 -0300

    atualização avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,23 +1,27 @@
-import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
-import { Image } from "react-native";
-import { theme } from "../../global/styles/theme";
-import { styles } from "./styles";
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image } from 'react-native';
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
 
 type Props = {
-    urlImage: string;
+  urlImage: string;
 }
 
 export function Avatar({ urlImage }: Props) {
-    return (
-        <LinearGradient
-            style={styles.container}
-            colors={[theme.colors.secondary50, theme.colors.secondary70]}
-        >
-            <Image
-                source={{uri: urlImage}}
-                style={styles.avatar}
-            />
-        </LinearGradient>
-    )
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary50, secondary70]}
+    >
+      <Image 
+        source={{ uri: urlImage }}
+        style={styles.avatar}
+      />
+    </LinearGradient>
+  )
+
 }
