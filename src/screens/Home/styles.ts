d6f4317e6bbb46f301837489dfb5e87953b2374f
ATCamposs <andre.tatibano@gmail.com>
2021-07-27 20:30:05 -0300

    feat(src/screens): update das screens

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,19 +1,20 @@
-import { StyleSheet } from "react-native";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        width: '100%',
-        paddingHorizontal: 24,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        //Margem no IO's conta a partir do topo da tela
-        //Margem no android conta a partir da parte "mais baixa"
-        //o react-native-iphone-x cuida disso.
-        marginTop: getStatusBarHeight() + 26,
-        marginBottom: 42
-    }
-})
+  container: {
+    flex: 1,
+  },
+  header: {
+    width: '100%',
+    paddingHorizontal: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: getStatusBarHeight() + 26,
+    marginBottom: 42,
+  },
+  matches: {    
+    marginTop: 24,
+    marginLeft: 24
+  }
+});
